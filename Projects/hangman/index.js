095aeedb847fdcb18a6aec3924c2a74b7aee1521
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-06 10:46:41 -0400

    switches from player 1 to player 2

--- Projects/hangman/index.js
+++ Projects/hangman/index.js
@@ -10,6 +10,7 @@ let p2_name;
 /*************Game Screen Controls***************************/
 let game_screen = document.querySelector("#game_screen");
 let start_btn = document.querySelector("#start_btn");
+let guesses = document.querySelector("#guesses");
 
 //left and right side of the screen that displays p1 and p2 name + time
 let player_nameDisplay = document.querySelectorAll(".display");
@@ -70,6 +71,7 @@ start_btn.onclick = function (){
     word = word[0].toUpperCase();
     word_array = word.split("");
     console.log(word);
+
     //glows player 1 name to indicate player 1 turn
     player_nameDisplay[0].classList.add("glow");
 
@@ -79,81 +81,147 @@ start_btn.onclick = function (){
         time1++
     },1000)
 
-    let keys = document.querySelectorAll(".btn_key");
     currPlay1 = true;
+    let keys = document.querySelectorAll(".btn_key");
+    console.log(keys);
     for (let i = 0; i < keys.length; i++){
-            keys[i].onclick = function (event){
-                if (currPlay1){
-                    let match = letterMatch(event.target.id)
-                    if (match == true){
-                        currPlay2 = false;
-                    }
-                    else{
-                        currPlay2 = true;
-                        if (currPlay2){
-                            letterMatch(event.target.id);
-                            currPlay1 = false;
-                            switchPlayer(player_nameDisplay[0],player_nameDisplay[1],stop_time1)
-                            stop_time2 = setInterval (()=> {
-                                time(time2,p2_time)
-                                time2++
-                            },1000)
-                        }
-                        else{
-                            currPlay1 = true
-                            switchPlayer(player_nameDisplay[1],player_nameDisplay[0],stop_time2)
-
-                        }
-                    }
-                }
-              /*PLAYER 1 */
-                    //if there is a letter match 
-                    //player 1 continues
-                    //give them glow class
-                    //chnage keyboard key text color for player 1
-                    //remove glow class from player 2
-                    //stop player 2 timer
-                    // currplay2 is false
-
-                /*PLAYER 2 */
-                    //lettermatch
-                    // if there is a letter match 
-                    //player 2 contnues 
-                    //give player 2 glow class
-                    //change keyboard color text for player 2
-                    //remove glow class from player 1
-                    //stop player 1 timer
-                    //currPlay1 is false
-
-    
-    
-                /*Game Logic 
-                    1) player one clicks start button on game screen
-                    2) once start button is clicked player ones timer starts
-                        --> when player 1 clicks a letter and the letter is in
-                            the word player 1 continues to play and the timer continues
-                        --> if the letter they click is not in the word their timer stops
-                        and its player 2 turn
-                    3)As soon as player 1 turns over player 2 turn starts and player 2 timer
-                        --> player two goes and clicks a letter if letter in word they continue
-                        to play and their timer continues. 
-                        --> if letter clicked not in word then timer stops and its player1
-                            turn
-                    4) this continues until one player continously guesses letter correct 
-                    and if they continously guess the letter correct and guess the whole word
-                    they win and game ends
-                    5)when game ends they are given the option to restart
-                */
-
-
-
-
-            }
-        }
-
-
+        keys[i].addEventListener ("click", play )
+    }
 
 }
+
+
+function play(event){
+    console.log (event.target)
+    if (currPlay1){
+        let match = letterMatch(event.target.id)
+        console.log(match);
+        if (match === 0){
+            currPlay2 = false;
+        }
+        else{// this means a match is 0 so letter does not match and we need to switch players
+            stop_time2 = setInterval (()=> {
+                time(time2,p2_time)
+                time2++
+            },1000)
+            currPlay2 = true;
+            switchPlayer (player_nameDisplay[0],player_nameDisplay[1],stop_time1)
+            match = letterMatch(event.target.id)
+            if (match === 0){
+                currPlay1 = false;
+            }
+            else{
+                currPlay1 = true;
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+       /* if (currPlay1){ 
+            let match = letterMatch(event.target.id)
+            if (match === 0){ //letter was found in word
+                currPlay2 = false;
+            }
+            else{
+
+                console.log("first else")
+                currPlay2 = true;
+                if (curr)
+                currPlay1 = false
+                console.log(match);
+                switchPlayer(player_nameDisplay[0],player_nameDisplay[1],stop_time1)
+                stop_time2 = setInterval (()=> {
+                    time(time2,p2_time)
+                    time2++
+                },1000)
+                match = letterMatch(event.target.id)
+                if (match === 0){
+                    currPlay1 = false
+                }
+                else{
+                    console.log("last else")
+                    currPlay1 = true;
+                    switchPlayer(player_nameDisplay[1],player_nameDisplay[0],stop_time2)
+                }
+            }
+        }*/
+    
+        /* if (currPlay1){
+            let match = letterMatch(event.target.id)
+            console.log(match);
+            if (match == 0){
+                currPlay2 = false;
+            }
+        }
+        currPlay2 = true;
+        else{
+            if (currPlay2){
+                letterMatch(event.target.id);
+                currPlay1 = false;
+                switchPlayer(player_nameDisplay[0],player_nameDisplay[1],stop_time1)
+                stop_time2 = setInterval (()=> {
+                    time(time2,p2_time)
+                    time2++
+                },1000)
+            }
+            else{
+                currPlay1 = true
+                switchPlayer(player_nameDisplay[1],player_nameDisplay[0],stop_time2)
+
+            }
+        }*/
+        /*PLAYER 1 */
+            //if there is a letter match 
+            //player 1 continues
+            //give them glow class
+            //chnage keyboard key text color for player 1
+            //remove glow class from player 2
+            //stop player 2 timer
+            // currplay2 is false
+
+        /*PLAYER 2 */
+            //lettermatch
+            // if there is a letter match 
+            //player 2 contnues 
+            //give player 2 glow class
+            //change keyboard color text for player 2
+            //remove glow class from player 1
+            //stop player 1 timer
+            //currPlay1 is false
+
+
+
+        /*Game Logic 
+            1) player one clicks start button on game screen
+            2) once start button is clicked player ones timer starts
+                --> when player 1 clicks a letter and the letter is in
+                    the word player 1 continues to play and the timer continues
+                --> if the letter they click is not in the word their timer stops
+                and its player 2 turn
+            3)As soon as player 1 turns over player 2 turn starts and player 2 timer
+                --> player two goes and clicks a letter if letter in word they continue
+                to play and their timer continues. 
+                --> if letter clicked not in word then timer stops and its player1
+                    turn
+            4) this continues until one player continously guesses letter correct 
+            and if they continously guess the letter correct and guess the whole word
+            they win and game ends
+            5)when game ends they are given the option to restart
+        */
+
+
+
+
+
+
 //takes in player 1 and 2 display, also takes in time of player that answered wrong 
 function switchPlayer(curr1_display, curr2_display,time){ 
         curr1_display.classList.remove("glow");
@@ -163,24 +231,12 @@ function switchPlayer(curr1_display, curr2_display,time){
 function letterMatch(event) {
     temp = false;
     for (let i = 0; i < word_array.length; i ++){
-        console.log(word_array[i],event);
         if (word_array[i] === event){
             temp = true;
             let p = document.createElement("p");
             p.innerHTML = event;
             word_dashes.children[i].replaceWith(p);
         }
-       //else{ // all this goes in the 
-            //console.log("letter not in word")
-            /*stop_time(stop_time1);
-            player_nameDisplay[0].classList.remove("glow");
-            player_nameDisplay[1].classList.add("glow");
-            stop_time2 = setInterval (()=> {
-                time(time2,p2_time)
-                time2++
-            },1000)*/
-      //  }
-
     }
     if (temp){
         return 0;
@@ -188,65 +244,6 @@ function letterMatch(event) {
     else{
         return 1;
     }
-    //check if its true and then it means char is in there //return 0 is temp is false
-    //outside this if return 1
-
-
-    /*word_array.filter(each => {
-        if (each !== event){
-            return each;
-        }
-        console.log(each.indexOf(each));
-    })*/
-
-    /*console.log(event,word_array);
-    //word_arrya.filter(key =>{ if })
-    if (word_array.includes(event)){
-        let p = document.createElement("p");
-        p.innerHTML = event;
-        let index = word_array.indexOf(event);
-        console.log(index);
-        console.log(word_dashes.children[index]);
-        word_dashes.children[index].replaceWith(p)
-        letters += event
-        return 1;
-    }*/
-    /*else{
-        stop_time(stop_time1);
-            player_nameDisplay[0].classList.remove("glow");
-            player_nameDisplay[1].classList.add("glow");
-            stop_time2 = setInterval (()=> {
-                time(time2,p2_time)
-                time2++
-            },1000)
-    }*/
-    //random_word=PRAISE
-    /*letter += letter;
-    //letter = ""+P
-    console.log("this is random word: ",random_word[0])
-    console.log("this is letter: ",letter[0]);
-    if (random_word[0] == letter[0]){
-        console.log("hello");
-    }
-    
-    for (let i = 0; i < word_array.length; i ++){
-        console.log(word_array[i]);
-        if (word_array[i] === letter){
-            let p = document.createElement("p");
-            p.innerHTML = letter;
-            word_dashes.children[i].replaceWith(p);
-            break;
-        }
-        else{
-            stop_time(stop_time1);
-            player_nameDisplay[0].classList.remove("glow");
-            player_nameDisplay[1].classList.add("glow");
-            stop_time2 = setInterval (()=> {
-                time(time2,p2_time)
-                time2++
-            },1000)
-        }
-    }*/
 }
 
 
@@ -327,10 +324,18 @@ function body(){
     ctx.stroke();
 }
 
-//head();
+function leftArm(){
 
+}
+function rightArm (){
 
+}
+function leftLeg(){
 
+}
+ function rightLeg(){
+
+ }
 
 
 
