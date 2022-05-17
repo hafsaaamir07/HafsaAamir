4e8b49b112851005a47182284e55c2c765b5717e
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-16 20:22:19 -0400

    completed magic 8 ball

--- Homework/HW6/server.js
+++ Homework/HW6/server.js
@@ -12,6 +12,16 @@ app.get("/tip/:total/:tipPercentage", (req,res) => {
     res.send(`<h1>Your tip will be... ${tot}</h1>`);
 })
 
+const arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", 
+"As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+"Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", 
+"My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get("/magic/:phrase", (req,res) => {
+    let random = parseInt(Math.random()*arr.length)
+    res.send (`<h3>${req.params.phrase}... ${arr[random]}</h3>`)
+
+})
 
 
 
