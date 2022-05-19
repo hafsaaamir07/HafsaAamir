6bf6791f06fdc809d2c5e81049aa8c1bb38683f3
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-19 15:12:04 -0400

    added middleware for body parser

--- Labs/first_api/app.js
+++ Labs/first_api/app.js
@@ -1,6 +1,13 @@
+
 const express = require ("express");
+//creates a varaible to store express methods 
 const app = express();
 
+//create a middleware to parse the request body
+app.use(express.json())
+
+
+//import router
 const learnerRouter = require("./routes/learnerRoutes")
 
 // Create a middleWare to handle route /api/v1/learners/
