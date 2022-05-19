148c9c73fdcaf1e65ef8a5f2deac3ca1b5f42fb2
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-19 06:50:06 -0400

    created a router to get single post

--- Labs/rest_tutorial/routes/posts.js
+++ Labs/rest_tutorial/routes/posts.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+//gets back all the posts
 router.get("/", async (req,res) => {
     try{
         const posts = await Post.find()
@@ -11,6 +12,8 @@ router.get("/", async (req,res) => {
         res.json({message: err})
     }
 })
+
+//submits a posts
 router.post("/", async (req,res) => {
     // calling this to create a new object
     const post = new Post({
@@ -25,7 +28,17 @@ router.post("/", async (req,res) => {
     catch(err){
         res.json({message: err});
     }
-    
+})
+
+//get a specific post
+router.get("/:postId", async (req,res) =>{
+    try{
+        const post = await Post.findById(req.params.postId)
+        res.json(post)
+    }
+    catch(err){
+        res.json({message:err})
+    }
 
 })
     
