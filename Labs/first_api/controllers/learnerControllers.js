a45eb1f75ce29b61bcd15afb9e835ce16e0de236
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-19 16:00:13 -0400

    created way to update and and delete learner

--- Labs/first_api/controllers/learnerControllers.js
+++ Labs/first_api/controllers/learnerControllers.js
@@ -4,7 +4,6 @@
 
 // This is to retrieve all the learners at once
 exports.getAllLearners =  async (request, response) => {
-
   try{
     //get all learners
     const learners = await Learner.find();
@@ -13,7 +12,7 @@ exports.getAllLearners =  async (request, response) => {
       response.status(200).json({
         status: "sucsess",
         data: {
-          learners: learners,
+          learners,
         },
       });
   }
@@ -23,17 +22,17 @@ exports.getAllLearners =  async (request, response) => {
       message: error,
     })
   }
-  };
+};
   
 // This controller is to create a new learner
 exports.createLearner = async (request, response) => {
   try {
-      //create our first learner
+    //create our first learner
     const newLearner = await Learner.create(request.body)
     response.status(201).json({
       status: "success",
       data: {
-        newLearner: newLearner,
+        newLearner,
       },
     });
   }
@@ -43,7 +42,7 @@ exports.createLearner = async (request, response) => {
       message: error,
     })
   }
-  };
+};
   
 // This controller is to retrieve a single learner
 exports.getSingleLearner = async (request, response) => {
@@ -53,36 +52,54 @@ exports.getSingleLearner = async (request, response) => {
       response.status(200).json({
       status: "success",
       data: {
-        learner: learner,
+        learner,
       },
+    });
+  }
+  catch(error){
+    response.status(500).join({
+      status: "error",
+      message: error,
+
+    })
+  }
+};
+  
+// This controller is to update a single learner data
+exports.updateLearner = async (request, response) => {
+  try {
+    //find and upadte learner
+    //firsdt param takes the id, then the patch method, third will return the updated user
+    const updateLearner = await Learner.findByIdAndUpdate(request.params.id, request.body, {new: true});
+    response.status(200).json({
+      status: "sucess",
+      data: {
+        updateLearner,
+      },
+    });
+  }
+  catch(error){
+    response.status(500).join({
+      status: "error",
+      message: error,
+    })
+
+  } 
+};
+
+// This controller is to delete a single learner
+exports.deleteLearner = async (request, response) => {
+  try{
+    await Learner.findByIdAndDelete(request.params.id)
+    response.status(204).json({
+      status: "fail",
     });
   }
   catch(error){
     response.status(500).join({
       stauts: "error",
       message: error,
-
     })
-  }
-  };
-  
-// This controller is to update a single learner data
-exports.updateLearner = (request, response) => {
-    response.status(500).json({
-      status: "fail",
-      data: {
-        message: "undefined routes",
-      },
-    });
-  };
-
-// This controller is to delete a single learner
-exports.deleteLearner = (request, response) => {
-    response.status(500).json({
-      status: "fail",
-      data: {
-        message: "undefined routes",
-      },
-    });
-  };
+  }  
+};
 
