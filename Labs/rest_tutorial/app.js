f46abf9399c64ad55be46851aa93892aacf023e8
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-19 05:22:37 -0400

    added body parser

--- Labs/rest_tutorial/app.js
+++ Labs/rest_tutorial/app.js
@@ -1,19 +1,14 @@
 const express = require ("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
 require ("dotenv/config");
- //Middlewares
-/*app.use("/posts", () =>{
-    console.log("This is a middleware running");
-})*/
 
+
+app.use(bodyParser.json());
 //Import Routes
 const postsRoute = require("./routes/posts")
 app.use ("/posts", postsRoute);
-
-
-
-
 
 
 //Routes
