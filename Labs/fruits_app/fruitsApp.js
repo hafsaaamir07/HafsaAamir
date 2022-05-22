46cd65c87891973a1a29dccce2ab60102d6c32e4
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-22 16:48:12 -0400

    rendred index + added missing port

--- Labs/fruits_app/fruitsApp.js
+++ Labs/fruits_app/fruitsApp.js
@@ -3,15 +3,20 @@ const app = express();
 require("dotenv").config();
 
 app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views"),createEngine());
+app.engine("jsx",require("express-react-views").createEngine());
 
 
-const fruitData = require (".models/fruits");
+const fruitData = require ("./models/fruits");
 
 app.get("/fruits/",(req,res) => {
-    res.send(fruitData);
+    res.render("Index", {fruits: fruitData});
 });
 
 app.get("/fruits/:indexOfFruitsArray", (req,res) =>{
-    res.send(fruitDta[req.params.indexOfFruitsArray]);
+    //pass the file name with view, callback
+    res.render("Show", {fruit: fruitData[req.params.indexOfFruitsArray]});
 });
+
+app.listen(3000, ()=> {
+    console.log(`Currently listening on PORT ${process.env.PORT}`)
+})
