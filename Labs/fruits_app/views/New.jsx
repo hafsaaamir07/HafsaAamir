94e2b6f108b4671ffe9dac2d366994a6310b736c
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-22 17:37:32 -0400

    POST request using form

---
+++ Labs/fruits_app/views/New.jsx
@@ -0,0 +1,23 @@
+const React = require("react")
+
+class New extends React.Component{
+    render(){
+        return (
+            <>
+                <form action ="/fruits" method="POST">
+                    Name: <input type="text" 
+                    name="name"/><br/>
+                    Color: <input type="text"
+                    name="color"/><br/>
+                    Is Ready To Eat: <input 
+                    type="checkbox" name="readyToEat"/><br/>
+                    <input type="submit" name=""
+                    value="Create Fruit"/>                    
+                </form>
+            </>
+        )
+    }
+
+}
+
+module.exports = New;
