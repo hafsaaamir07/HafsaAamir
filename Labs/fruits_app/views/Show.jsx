4fd8a0961952e540f680cffbcdb415e63ea3c17c
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-22 16:48:46 -0400

    cleaned up code

--- Labs/fruits_app/views/Show.jsx
+++ Labs/fruits_app/views/Show.jsx
@@ -2,10 +2,20 @@ const React = require("react");
 
 class Show extends React.Component {
     render() {
+
+        const fruit = this.props.fruit
         return (
-            <h1> Show Page</h1>
+            <>
+                <h1> Show Page</h1>
+                The {fruit.name} is {fruit.color} {" "}
+                {fruit.readyToEat? "It is ready to eat" : "NahhMann, thats Expired"}
+            
+            </>
         );
     }
 }
 
 module.exports = Show;
+
+
+//inside react component: render and :return
