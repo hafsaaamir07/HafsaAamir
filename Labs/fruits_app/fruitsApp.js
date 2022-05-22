2f0ede32a0e78f2f2bfdbcb5b2291c63d6f4985c
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-22 17:38:31 -0400

    added POST route

--- Labs/fruits_app/fruitsApp.js
+++ Labs/fruits_app/fruitsApp.js
@@ -8,14 +8,22 @@ app.engine("jsx",require("express-react-views").createEngine());
 
 const fruitData = require ("./models/fruits");
 
-app.get("/fruits/",(req,res) => {
+app.get("/fruits",(req,res) => {
     res.render("Index", {fruits: fruitData});
 });
+app.get("/fruits/new", (req,res) => {
+    res.render("New")
+})
 
 app.get("/fruits/:indexOfFruitsArray", (req,res) =>{
     //pass the file name with view, callback
     res.render("Show", {fruit: fruitData[req.params.indexOfFruitsArray]});
 });
+
+// post request
+app.post("/fruits", (req,res) => {
+    res.send("hi");
+})
 
 app.listen(3000, ()=> {
     console.log(`Currently listening on PORT ${process.env.PORT}`)
