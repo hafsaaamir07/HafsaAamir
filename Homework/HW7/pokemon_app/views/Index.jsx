931cfa2d22c3f55bae1ed783bf61a19763d25998
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-23 15:16:48 -0400

    index.jsx view rendered at pokemon route

---
+++ Homework/HW7/pokemon_app/views/Index.jsx
@@ -0,0 +1,20 @@
+const React = require("react")
+const myStyle = {
+    color: "#FFFFFF",
+    backgroundColor: "#ADD8E6",
+};
+
+class Index extends React.Component {
+    render () {
+        return (
+            <>
+            <h1>See All Pokemon's</h1>
+            <div style = {myStyle}>
+
+
+            </div>
+            </>
+        )
+    }
+}
+module.exports = Index;
