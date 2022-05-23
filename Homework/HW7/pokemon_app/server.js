ca4e184aa685ee0908b72b1aa9325334fc726844
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-23 15:17:23 -0400

    index.jsx view rendered at pokemon route

--- Homework/HW7/pokemon_app/server.js
+++ Homework/HW7/pokemon_app/server.js
@@ -5,12 +5,15 @@ const PORT = 3000;
 const pokemon = require("./models/pokemon")
 
 
+app.set("view engine","jsx");
+app.engine("jsx",require("express-react-views").createEngine());
+
 app.get("/", (req,res) => {
     res.send("Welcome to the Pokemon App!");
 })
 
 app.get("/pokemon", (req,res) =>{
-    res.send(pokemon)
+    res.render("Index", {pokemons: pokemon});
 })
 
 app.listen(3000, () => {
