30bea51482231636b5626d35c831cb0ec54825b5
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-24 22:13:31 -0400

    show views shows req.params.id

--- Homework/HW7/pokemon_app/server.js
+++ Homework/HW7/pokemon_app/server.js
@@ -4,7 +4,6 @@ const PORT = 3000;
 
 const pokemon = require("./models/pokemon")
 
-
 app.set("view engine","jsx");
 app.engine("jsx",require("express-react-views").createEngine());
 
@@ -16,6 +15,10 @@ app.get("/", (req,res) => {
 
 app.get("/pokemon", (req,res) =>{
     res.render("Index", {pokemons: pokemon});
+})
+
+app.get("/pokemon/:id",(req,res) => {
+    res.send(req.params.id);
 })
 
 app.listen(3000, () => {
