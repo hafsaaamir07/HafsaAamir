e4eb1a5ae99a8e1bd0b0dc3ab800f391009b7aff
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-27 02:22:04 -0400

    refactored controllers

--- Projects/CRS/app.js
+++ Projects/CRS/app.js
@@ -8,10 +8,54 @@ require("dotenv").config();
 
 app.set("view engine","ejs");
 
-//create route handlers
-app.get("/admin/course",(req,res) => {
-    res.send()
+
+
+//refactor controllers:
+const getAllCourses = (req, res) => {
+    res.send("index page")
+}
+const createCourse = (req,res) {
+    res.send("post request")
+}
+
+const getSingleCourse = (req,res) => {
+    res.send(req.params.id)
+}
+
+const updateCourse = (req,res) => {
+    res.send("update course")
+}
+
+const deleteCourse = (req,res) => {
+    res.send("deleted course")
+}
+// index page: list of course
+app.get("/admin/courses", getAllCourses)
+
+//shows page: individual course
+app.get("/admin/courses/:id", getSingleCourse)
+
+//new page: allows to create a new course
+app.get("/admin/courses/new", (req,res) => {
+    res.send("new page")
 })
+
+//edit page: allows us to edit a single course
+app.get("/admin/courses/:id/edit", (req,res) => {
+    res.send("edit page")
+})
+//delete route
+app.delete("/admin/courses/:id", deleteCourse)
+
+//update a course
+app.patch("/admin/course/:id", updateCourse)
+
+//creates a new course
+app.post("/admin/course/", createCourse)
+
+
+
+
 
 
 
