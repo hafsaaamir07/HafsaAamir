912ce298b34a620c5a80eaf00a332b4c3649ea9c
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-29 01:24:34 -0400

    imported course controllers

--- Projects/CRS/courseApp.js
+++ Projects/CRS/courseApp.js
@@ -4,49 +4,24 @@ const express = require("express");
 //create a variable to store our express methods
 const app = express();
 
+//import course controller here
+const courseController = require("./controllers/courseController");
+
 //setting our view engine to ejs
 app.set("view engine","ejs");
 
-                                                /**************refactoring route controllers**************/
-// this is to retrieve all the courses at once
-const getAllCourses = (req,res) => {
-    res.render("index")
-}
-//this is to create a a new course
-const createCourse = (req,res) => {
-    res.send("created a new course")
-}
-//this is to retrieve a single course
-const showCourse = (req,res) => {
-    res.render("show")
-}
-//this initiates a form request to update a single course
-const updateCourse = (req,res) => {
-    res.send("updated ",req.params.id)
-}
-//this initiates a form request to delete a single course
-const deleteCourse =  (req,res) => {
-    res.send("delete ",req.params.id)
-}
-//this initiate a form request to create a new course
-const newCourse = (req,res) => {
-    res.render("new")
-}
-//this is to edit a single course
-const editCourse = (req,res) => {
-    res.render("edit")
-}
+
 //create an exppress router 
 const courseRouter = express.Router();
                                                             /**************middleware**************/
 //creating middleware to handle routes
-app.use("/admin/courses", courseRouter)
+app.use("/admin/courses", courseRouter);
                                                        /*************refactor our route handlers*************/
 //base route
-courseRouter.route("/").get(getAllCourses).post(createCourse) // if we do "/admin/courses" plus "/" will take me to these routes
-courseRouter.route("/:id").get(showCourse).patch(updateCourse).delete(deleteCourse)
-courseRouter.route("/new").get(newCourse)
-courseRouter.route("/:id/edit").get(editCourse)
+courseRouter.route("/").get(courseController.getAllCourses).post(courseController.createCourse) // if we do "/admin/courses" plus "/" will take me to these routes
+courseRouter.route("/:id").get(courseController.showCourse).patch(courseController.updateCourse).delete(courseController.deleteCourse)
+courseRouter.route("/new").get(courseController.newCourse)
+courseRouter.route("/:id/edit").get(courseController.editCourse)
 
 
                                                    /*************creating route handlers
