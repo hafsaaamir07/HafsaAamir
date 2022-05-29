f3aa8a02b2246547c686aa35f307d1965adad182
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-29 02:44:24 -0400

    moved route handlers to routes folder

---
+++ Projects/CRS/routes/courseRoutes.js
@@ -0,0 +1,23 @@
+//import express to create express routers
+const express = require("express");
+const { model } = require("mongoose");
+
+//create an express router
+const courseRouter = express.Router();
+
+                                                        /*route handlers*/
+//base route
+courseRouter.route("/")
+    .get(courseController.getAllCourses)
+    .post(courseController.createCourse) // if we do "/admin/courses" plus "/" will take me to these routes
+courseRouter.route("/:id")
+    .get(courseController.showCourse)
+    .patch(courseController.updateCourse)
+    .delete(courseController.deleteCourse)
+courseRouter.route("/new")
+    .get(courseController.newCourse)
+courseRouter.route("/:id/edit")
+    .get(courseController.editCourse)
+
+//export our router to be used in other parts of the our application
+model.exports = router;
