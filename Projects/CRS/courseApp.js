d04745e5e9e0d773c3e59a2d71c1c141977215fd
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-05-29 02:47:34 -0400

    imported course routes

--- Projects/CRS/courseApp.js
+++ Projects/CRS/courseApp.js
@@ -7,21 +7,31 @@ const app = express();
 //import course controller here
 const courseController = require("./controllers/courseController");
 
+//import course routes
+const courseRoutes = require("./routes/courseRoutes")
+
 //setting our view engine to ejs
 app.set("view engine","ejs");
 
 
 //create an exppress router 
 const courseRouter = express.Router();
-                                                            /**************middleware**************/
+
 //creating middleware to handle routes
 app.use("/admin/courses", courseRouter);
-                                                       /*************refactor our route handlers*************/
+
+
+/*************refactor our route handlers**********
 //base route
-courseRouter.route("/").get(courseController.getAllCourses).post(courseController.createCourse) // if we do "/admin/courses" plus "/" will take me to these routes
-courseRouter.route("/:id").get(courseController.showCourse).patch(courseController.updateCourse).delete(courseController.deleteCourse)
+courseRouter.route("/")
+    .get(courseController.getAllCourses)
+    .post(courseController.createCourse) // if we do "/admin/courses" plus "/" will take me to these routes
+courseRouter.route("/:id")
+    .get(courseController.showCourse)
+    .patch(courseController.updateCourse)
+    .delete(courseController.deleteCourse)
 courseRouter.route("/new").get(courseController.newCourse)
-courseRouter.route("/:id/edit").get(courseController.editCourse)
+courseRouter.route("/:id/edit").get(courseController.editCourse) *//////
 
 
                                                    /*************creating route handlers
