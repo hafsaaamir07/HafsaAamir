e39a3a86c115873eb1551bdfeb9c5a7e550002dc
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-01 00:55:32 -0400

    fixed up routes

--- Projects/CRS/routes/courseRoutes.js
+++ Projects/CRS/routes/courseRoutes.js
@@ -1,6 +1,6 @@
 //import express to create express routers
 const express = require("express");
-const { model } = require("mongoose");
+const app = require("../courseApp");
 
 //create an express router
 const courseRouter = express.Router();
@@ -10,18 +10,22 @@ const courseController = require("./../controllers/courseController")
 
                                                         /*route handlers*/
 //base route
+//post request for login 
 courseRouter.route("/")
-    .get(courseController.getAllCourses)
-    .post(courseController.createCourse) // if we do "/admin/courses" plus "/" will take me to these routes
-courseRouter.route("/:id")
-    .get(courseController.showCourse)
-    .patch(courseController.updateCourse)
-    .delete(courseController.deleteCourse)
+    .get(courseController.getAllCourses) // if we do "/admin/courses/:id" plus "/" will take me to these routes
+    .post(courseController.createCourse) 
+courseRouter.route("/login")
+    .post(courseController.adminLogin)
 courseRouter.route("/new")
     .get(courseController.newCourse)
+courseRouter.route("/:id")
+    .get(courseController.showCourse)
+    .delete(courseController.deleteCourse)
 courseRouter.route("/:id/edit")
     .get(courseController.editCourse)
+    .patch(courseController.updateCourse)
 
 //export our router to be used in other parts of the our application
 // this is a single export
-model.exports = courseRouter;
+module.exports = courseRouter;
+
