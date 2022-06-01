89d5753cfcd2bec59ff729c2e4e5bc8ff4bd72d1
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-01 00:54:52 -0400

    cleaned up schema

--- Projects/CRS/models/courseModel.js
+++ Projects/CRS/models/courseModel.js
@@ -3,46 +3,21 @@ const mongoose = require("mongoose");
 
 //create our course schema
 const courseSchema = new mongoose.Schema({
-    Course_Name:{
-        type: String,
-    },
-    Course_Id: {
-        type: String,
-    },
-    Units: {
-        type: Number,
-    },
-    Semester_Level: {
-        type: String,
-    },
-    Prereq: {
-        type: String,
-    },
-    Course_Description: {
-        type: String,
-    },
-    Max_Students:{
-        type: Number,
-    },
-    Curr_Students: {
-        type: Number,
-    },
-    Student_List: {
-        type: [String],
-    },
-    Instructor: {
-        type: String,
-    },
-    Section_Number: {
-        type: Number,
-    },
-    Location:{
-        type: String,
-    },
-    Headshot:{
-        type: String
-    }
+    Course_Name:{ type: String, },
+    Course_Id: { type: String, },
+    Units: { type: Number, },
+    Semester_Level: { type: String, },
+    Prereq: { type: String, },
+    Course_Description: { type: String, },
+    Max_Students:{ type: Number, },
+    Curr_Students: { type: Number, },
+    Student_List: { type: [String], },
+    Instructor: { type: String, },
+    Section_Number: { type: Number, },
+    Location:{ type: String, },
+    Headshot:{ type: String }
 })
+
 
 const Courses = mongoose.model("Courses",courseSchema);
 
