de4c719f8fd27d32e284ddc8a5fb2cf85377484f
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-14 00:14:40 -0400

    set up movie component

---
+++ Labs/react_movies/react_movies/src/components/MovieInfo.jsx
@@ -0,0 +1,17 @@
+// 1. import react
+import React from "react"
+
+// 2. additional imports
+
+// 3. create component
+class MovieInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Movie Component</h1>
+            </div>
+        )
+    }
+}
+// 4. export component
+export default MovieInfo
