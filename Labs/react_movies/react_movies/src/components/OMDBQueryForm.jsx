9c9ee03da7226584dc1541ab22efeb94a6944677
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-14 05:08:29 -0400

    refactored code

---
+++ Labs/react_movies/react_movies/src/components/OMDBQueryForm.jsx
@@ -0,0 +1,78 @@
+// 1. import react
+import React from "react"
+
+// 2. additional imports
+import MovieInfo from "./MovieInfo";
+
+// 3. create component
+class OMBDQueryForm extends React.Component{
+    //add a state to our component
+    state = {
+        baseURL: 'http://www.omdbapi.com/?',
+        apikey: 'apikey=' + '910e0323',
+        query: '&t=',
+        movieTitle: '',
+        searchURL: ''
+    };
+    //methods
+    // handles input bar
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value,
+        });
+    };
+    //submit form after we enter name of movie
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        this.setState({
+          searchURL: 
+          this.state.baseURL + 
+          this.state.apikey + 
+          this.state.query + 
+          this.state.movieTitle
+        },
+            () => {
+                fetch(this.state.searchURL)
+                .then ((response) => {
+                    return response.json();
+                })
+                .then ((data) => {
+                    this.setState({
+                        searchResult: data,
+                    });
+                })
+                .catch((error) => {
+                    console.error(error.message)
+                })
+                //this runs no matter what (wether you get data or not)
+                .finally(() => {
+                    this.setState({
+                        movieTitle: "",
+                    });
+                });
+            }
+        );
+    };
+      
+    render(){
+        return(
+            <>
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor='movieTitle'>Title</label>
+              <input 
+              id='movieTitle' 
+              type='text' 
+              value={this.state.movieTitle}
+              onChange={this.handleChange}/>{" "}
+              <input type='submit' value='Find Movie Info' 
+              disabled={!this.state.movieTitle ? true : false}/>
+            </form>
+            {/*if there is a serach result then pass in the movie component else an empty string*/}
+            {this.state.searchResult ? (<MovieInfo searchResult={this.state.searchResult}/>) : ("")}
+          </>
+        );
+    };
+};
+
+// 4. export component
+export default OMBDQueryForm;
