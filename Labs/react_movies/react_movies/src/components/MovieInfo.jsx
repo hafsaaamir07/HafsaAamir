eb16da44aaab2a5d83f095370264b36bc63ebd85
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-14 04:21:46 -0400

    displaying movie details to page using props

--- Labs/react_movies/react_movies/src/components/MovieInfo.jsx
+++ Labs/react_movies/react_movies/src/components/MovieInfo.jsx
@@ -8,7 +8,11 @@ class MovieInfo extends React.Component {
     render() {
         return (
             <div>
-                <h1>Movie Component</h1>
+                <h1>Title: {this.props.searchResult.Title}</h1>
+                <h2>Year: {this.props.searchResult.Year}</h2>
+                <img src={this.props.searchResult.Poster} alt={this.props.searchResult.Title}/>
+                <h3>Genre: {this.props.searchResult.Genre}</h3>
+                <h4>Plot: {this.props.searchResult.Plot}</h4>           
             </div>
         )
     }
