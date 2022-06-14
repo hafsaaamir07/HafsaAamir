d50b4f97c716c54bdbb4314f8f12045eca5a5a3f
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-14 10:43:37 -0400

    added middle ware to overide method in forms

--- Projects/CRS/courseApp.js
+++ Projects/CRS/courseApp.js
@@ -4,6 +4,8 @@ const express = require("express");
 //create a variable to store our express methods
 const app = express();
 
+var methodOverride = require('method-override')
+
 //import course routes
 const courseRoutes = require("./routes/courseRoutes")
 
@@ -16,6 +18,9 @@ app.set("view engine","ejs");
 //create a middleware to parse the request body
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
+// override with POST having ?_method=PATCH
+app.use(methodOverride('_method'))
+
 
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
