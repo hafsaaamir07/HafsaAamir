2d0f4ac02a2b92ca77047645cf050acdf4f284b2
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-15 00:34:48 -0400

    accessing data passed from parent to Child.jsx

--- Labs/react_function-component_review/src/components/Child.jsx
+++ Labs/react_function-component_review/src/components/Child.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Child = () => {
+const Child = (props) => { // you can so props or pass in {setVariable} directly to get the function
+    //we will see definition of the function (will be an object)
+    console.log(props.setVariable)
     return (
         <div>
-
         </div>
     );
 };
 
-export default Child;
+export default Child; 
