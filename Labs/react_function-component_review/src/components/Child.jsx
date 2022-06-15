55e6345ba33d13defc633c1e34d7f766cc423fd9
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-15 01:38:55 -0400

    added comments

--- Labs/react_function-component_review/src/components/Child.jsx
+++ Labs/react_function-component_review/src/components/Child.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Child = (props) => { // you can so props or pass in {setVariable} directly to get the function
     //we will see definition of the function (will be an object)
+    console.log(props) // will show all the data passed under props 
     console.log(props.setVariable)
+
     return (
         <div>
         </div>
