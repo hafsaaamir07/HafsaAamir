14632f762d14f43ad04f46e7991db71dea13d7c9
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-14 21:53:17 -0400

    set up index file to render to html

---
+++ Labs/react_function-component_review/src/index.jsx
@@ -0,0 +1,13 @@
+// 1. import react
+import React from "react";
+
+// 2. import reactdom
+import ReactDOM from "react-dom";
+
+// 3. additional imports
+import App from "./components/App";
+
+// 4. render components to the screen 
+ReactDOM.render(
+    <App />, document.querySelector("#root")
+);
