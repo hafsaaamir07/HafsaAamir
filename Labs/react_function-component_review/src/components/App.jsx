53180da79e2f56e2f365f0f1e9868a37a8fc2921
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-14 21:57:09 -0400

    practice using function component instead of class

---
+++ Labs/react_function-component_review/src/components/App.jsx
@@ -0,0 +1,17 @@
+// 1. import react
+import React from "react";
+
+// 2. additional import 
+
+
+// 3. create functional component
+const App = () => {
+    return(
+        <div style={{backgroundColor: "lightcoral"}}>
+            <h1>App Component</h1>
+        </div>
+    );
+};
+
+// 4. export component
+export default App;
