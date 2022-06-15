9a8b29270c8017d32a644765f3f952716702b72e
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-15 00:18:49 -0400

    passing data from parent to child component

--- Labs/react_function-component_review/src/components/App.jsx
+++ Labs/react_function-component_review/src/components/App.jsx
@@ -6,9 +6,17 @@ import React from "react";
 
 // 3. create functional component
 const App = () => {
+    //lets create a varaible 
+    let varaible;
+
+    //lets create a function 
+    const setVaribale = (data) => {
+        variable = data;
+    }
     return(
         <div style={{backgroundColor: "lightcoral"}}>
             <h1>App Component</h1>
+            <Child setVaribale={setVaribale} />
         </div>
     );
 };
