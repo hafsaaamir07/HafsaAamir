bfa7a7803af8d8375bb37c7087d1238df8a98081
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-15 10:12:17 -0400

    rendering app component to screen

---
+++ Labs/react_hooks/jsx/src/index.jsx
@@ -0,0 +1,13 @@
+//1 . import react
+import React from "react";
+
+// 2. import react-dom
+import ReactDOM from "react-dom";
+
+// 3. additional import
+import App from "./components/App";
+
+// 4. render component
+ReactDOM.render (
+    <App />,document.querySelector("#root")
+);
