e03bbb6792e9d1776e3d49a929412fc3727bfb5b
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-15 10:13:05 -0400

    set up App and passed in Counter component

---
+++ Labs/react_hooks/jsx/src/components/App.jsx
@@ -0,0 +1,16 @@
+// 1. import react
+import React from "react";
+
+// 2. additional imports
+import Counter from "./Counter";
+
+// 3.create component
+const App = () => {
+    return(
+        <div>
+            <Counter />
+        </div>
+    );
+};
+
+export default App;
