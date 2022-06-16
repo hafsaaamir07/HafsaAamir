6adf16a553dbfaa52a93e23b2ee2f14cb6dc7b61
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-16 11:33:33 -0400

    practice using useEffect

---
+++ Labs/react_hooks/jsx/src/components/FormComponent.jsx
@@ -0,0 +1,31 @@
+// 1. import react
+import React from "react";
+
+// 2. additional imports
+import { useRef } from "react";
+
+// 3. create component
+const FormComponent = () => {
+    //lets create our first refrence
+    const inputRef = useRef();
+
+    //create refrence to the pTag
+    const pTagRef = useRef();
+
+    //lets add event handler
+    const handleClick = () => {
+        console.log(inputRef.current.value)
+        console.log(pTagRef.current.textContent)
+        
+    }
+    return (
+        <div>
+            <input type="text" ref={inputRef}/>
+            <p ref={pTagRef}>This is a p tag</p>
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    );
+};
+
+// 4. export component
+export default FormComponent;
