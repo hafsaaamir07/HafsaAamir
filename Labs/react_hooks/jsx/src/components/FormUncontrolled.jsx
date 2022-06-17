ebb371e32bed7853698534ca86d2d9216d0a63ec
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-17 05:18:04 -0400

    created useRef and assigned to input tags

--- Labs/react_hooks/jsx/src/components/FormUncontrolled.jsx
+++ Labs/react_hooks/jsx/src/components/FormUncontrolled.jsx
@@ -6,13 +6,19 @@ import { useRef } from "react";
 
 // 3. create component
 const FormUncontrolled = () => {
+    //create refrences
+    const nameInputRef = useRef();
+
+    const ageInputRef = useRef();
+
+
     return(
         <div> 
             <h1 style={{color: "lightslategrey"}}>Form Uncontrolled</h1>
 
             <form>
-                <input type="text"  placeholder="write name here" /> {" "}
-                <input type="number"  placeholder="write age here" /> {" "}
+                <input type="text"  placeholder="write name here" ref={nameInputRef}/> {" "}
+                <input type="number"  placeholder="write age here" ref={ageInputRef}/> {" "}
                 <input type="submit" value="Submit Form" />
             </form>
             
