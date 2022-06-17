b07b0939888ca63c0c4e5ba590b0a6aa170c610b
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-17 03:42:18 -0400

    setup form

--- Labs/react_hooks/jsx/src/components/FormControlled.jsx
+++ Labs/react_hooks/jsx/src/components/FormControlled.jsx
@@ -2,12 +2,31 @@
 import React from "react";
 
 // 2. additional imports 
+import { useState } from "react";
 
 // 3. create component 
 const FormControlled = () => {
+    //create state
+    const [formState, setFormState] = useState({
+        name: "",
+        age: 0,
+    });
     return(
         <div>
             <h1 style={{color: "lightcoral"}}>Form Controlled</h1>
+            <form>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="write name here"
+                />{" "}
+                <input
+                    type="number"
+                    name="age"
+                    placeholder="write age here"
+                />
+                <input type="submit" value="Submit Form" />
+            </form>
         </div>
     );
 };
