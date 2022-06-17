9c20a3fcd38fb1eaf3632dedd09b969a1a7b7c10
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-17 05:05:12 -0400

    created FormUncontrolled Component

---
+++ Labs/react_hooks/jsx/src/components/FormUncontrolled.jsx
@@ -0,0 +1,16 @@
+// 1. import react 
+import React from "react";
+
+// 2. additional imports
+import { useRef } from "react";
+
+// 3. create component
+const FormUncontrolled = () => {
+    return(
+        <div> 
+            <h1 style={{color: "lightslategrey"}}>Form Uncontrolled</h1>
+        </div>
+    )
+}
+// 4. export component
+export default FormUncontrolled;
