07d6e28ef537b72e486e8e7ac0575a1da62e6024
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-17 03:23:20 -0400

    created new form component

---
+++ Labs/react_hooks/jsx/src/components/FormControlled.jsx
@@ -0,0 +1,15 @@
+// 1. import from react
+import React from "react";
+
+// 2. additional imports 
+
+// 3. create component 
+const FormControlled = () => {
+    return(
+        <div>
+            <h1>Form Controlled</h1>
+        </div>
+    )
+}
+// 4. export component
+export default FormControlled
