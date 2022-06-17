690ec3b18578037196ea159cc71c8518499e9393
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-17 04:09:48 -0400

    changing source of truth of input values from html to react

--- Labs/react_hooks/jsx/src/components/FormControlled.jsx
+++ Labs/react_hooks/jsx/src/components/FormControlled.jsx
@@ -11,6 +11,18 @@ const FormControlled = () => {
         name: "",
         age: 0,
     });
+
+    //create handleChange function for input values
+    const handleChange = (e) => {
+        setFormState({
+            /*if the sate is an object or array we have to pass a new array or object and 
+            not a modified version bc obj and arr are passed by refrence and we want to change the value*/
+
+            /*here we created a new object and spreaad the initial object, inour case being name and age*/
+            ...formState, [e.target.name] : e.target.value /*this will be assigned to the name state -> so 
+            whatever name is in the form that is the current target*/
+        })
+    }
     return(
         <div>
             <h1 style={{color: "lightcoral"}}>Form Controlled</h1>
@@ -19,11 +31,19 @@ const FormControlled = () => {
                     type="text"
                     name="name"
                     placeholder="write name here"
+                    onChange={(e) => {
+                        return handleChange(e)
+                    }}
+                    value={formState.name}
                 />{" "}
                 <input
                     type="number"
                     name="age"
                     placeholder="write age here"
+                    onChange={(e) => {
+                        return handleChange(e)
+                    }}
+                    value={formState.age}
                 />
                 <input type="submit" value="Submit Form" />
             </form>
