7cd6cf934dd69a9b067b76b05a72806f4011c7a4
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-17 05:22:13 -0400

    handling application from reloading when form is submitted

--- Labs/react_hooks/jsx/src/components/FormUncontrolled.jsx
+++ Labs/react_hooks/jsx/src/components/FormUncontrolled.jsx
@@ -6,17 +6,27 @@ import { useRef } from "react";
 
 // 3. create component
 const FormUncontrolled = () => {
+
     //create refrences
     const nameInputRef = useRef();
-
     const ageInputRef = useRef();
+
+    //Craete a function to submit our form
+    const handleFormSubmit = (e) => {
+        //prevent the form from reloading our application
+        e.preventDefault();
+        console.log({
+            name: nameInputRef.current.value,
+            age: ageInputRef.current.value,
+        })
+    }
 
 
     return(
         <div> 
             <h1 style={{color: "lightslategrey"}}>Form Uncontrolled</h1>
 
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <input type="text"  placeholder="write name here" ref={nameInputRef}/> {" "}
                 <input type="number"  placeholder="write age here" ref={ageInputRef}/> {" "}
                 <input type="submit" value="Submit Form" />
