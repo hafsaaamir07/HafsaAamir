78716a3e990680b657ccaf288b2a287cd906e230
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-21 15:59:17 -0400

    created and exported context

---
+++ Labs/state_management/ctx-provider_industry_std/src/contexts/CheeseContext.jsx
@@ -0,0 +1,9 @@
+// 1. import  create context
+import { createContext } from "react";
+
+// 2. create your context object
+const context = createContext(null); 
+//this is an object
+
+// 3. export context
+export default context;
