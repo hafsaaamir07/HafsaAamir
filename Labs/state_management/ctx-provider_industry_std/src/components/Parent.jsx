2581ff7fb19557d1538ec09322f38adc7295f876
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-21 16:03:20 -0400

    imported context and passing in data

--- Labs/state_management/ctx-provider_industry_std/src/components/Parent.jsx
+++ Labs/state_management/ctx-provider_industry_std/src/components/Parent.jsx
@@ -1,14 +1,17 @@
- // 1. import react 
+// 1. import react 
  import React from "react";
 
  // 2. additional imports 
  import Child from "./Child";
+ import CheeseContext from "./../contexts/CheeseContext"
 
  // 3. create component 
  const Parent = () => {
     return (
         <div>
-           <Child />
+         <CheeseContext.Provider value="cheese">
+            <Child />
+         </CheeseContext.Provider>
         </div>
     );
  };
