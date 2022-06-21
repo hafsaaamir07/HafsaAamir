022bb9a3110feb8aa5c2af4020f95eba31d9a51e
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-20 23:20:42 -0400

    renderinf to screen

---
+++ Labs/state_management/prop_drilling/src/index.jsx
@@ -0,0 +1,13 @@
+// 1. import react
+import React from "react";
+
+// 2. import react-dom
+import ReactDOM from "react-dom";
+
+// 3. additional import
+import Parent from "./components/Parent";
+
+// 4. render component
+ReactDOM.render (
+    <Parent />, document.querySelector("#root")
+);
