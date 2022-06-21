0e98af087a45e105b95abaf8836d34d536b9a6e1
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-20 22:43:40 -0400

    showing updated state

--- Labs/state_management/react_lifting_state/src/components/ReceiverChild.jsx
+++ Labs/state_management/react_lifting_state/src/components/ReceiverChild.jsx
@@ -4,11 +4,15 @@ import React from "react";
 // 2. additional imports 
 
 // 3. create component 
-const ReceieverChild = () => {
-    //create a piece of state
+const ReceieverChild = (props) => {
     return (
         <div>
-            <h1>Receiver Component</h1>
+            {/*first time the application loads it will show hello
+            because state is ("hello") 
+            then for our ReceiverChild we pass our state and the data is recevied as a props
+            the button in SenderChild updates the state*/}
+            <h1 style={{color: "lightcoral"}}>Receiver Component</h1>
+            <h2>{props.state}</h2>
         </div>
     )
 }
