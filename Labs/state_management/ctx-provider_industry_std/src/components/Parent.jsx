52879bdf1fadc5e30c8e993695ce8dcf9b9cbb2f
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-21 11:55:14 -0400

    returning child component

---
+++ Labs/state_management/ctx-provider_industry_std/src/components/Parent.jsx
@@ -0,0 +1,17 @@
+ // 1. import react 
+ import React from "react";
+
+ // 2. additional imports 
+ import Child from "./Child";
+
+ // 3. create component 
+ const Parent = () => {
+    return (
+        <div>
+           <Child />
+        </div>
+    );
+ };
+
+ // 4. export component 
+ export default Parent;
