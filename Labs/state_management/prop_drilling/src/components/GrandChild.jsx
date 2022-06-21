16c9762753fe2b09196141a85bd1d265574b9c8e
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-20 23:26:49 -0400

    craeted and setup component

---
+++ Labs/state_management/prop_drilling/src/components/GrandChild.jsx
@@ -0,0 +1,16 @@
+// 1. import react
+import React from "react";
+
+// 2. additional import
+
+// 3. create component
+const Grandchild= () => {
+    return (
+        <div>
+            <h1>Grandchild Component</h1>
+        </div>
+    );
+};
+
+// 4. export component
+export default Grandchild;
