daa16cd8d918113686f7fcddfab833d37fe7e862
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-21 16:16:00 -0400

    displaying context to screen

--- Labs/state_management/ctx-provider_industry_std/src/components/GrandChild.jsx
+++ Labs/state_management/ctx-provider_industry_std/src/components/GrandChild.jsx
@@ -1,13 +1,17 @@
  // 1. import react 
- import React from "react";
+ import React, {useContext} from "react";
 
  // 2. additional imports 
+ import CheeseContext from "./../contexts/CheeseContext"
 
  // 3. create component 
  const GrandChild = () => {
+   //lets retrieev our context
+   const ctx = useContext(CheeseContext);
     return (
         <div>
            <h1>GrandChild Component</h1>
+           <h3>{ctx}</h3>
         </div>
     );
  };
