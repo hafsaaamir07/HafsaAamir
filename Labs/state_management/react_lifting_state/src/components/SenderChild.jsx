20c81d4c61ff8b33b29893de5086851d8459d78d
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-20 21:27:51 -0400

    created new Send component

---
+++ Labs/state_management/react_lifting_state/src/components/SenderChild.jsx
@@ -0,0 +1,17 @@
+// 1.import from react 
+import React from "react";
+
+// 2. additional imports 
+
+// 3. create component 
+const SenderChild = () => {
+    //create a piece of state
+    return (
+        <div>
+            <h1>Sender Component</h1>
+        </div>
+    );
+};
+
+// 4. export component
+export default SenderChild;
