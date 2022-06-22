ad817174c2aa0224c88ef0781f8d273e4353f5bf
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-21 21:20:46 -0400

    using useState to map data and display it

---
+++ Labs/shopping_cart/store/src/components/ShoppingList.jsx
@@ -0,0 +1,27 @@
+// 1. import react 
+import React from "react";
+
+// 2. additional imports
+import Data from "./Data";
+import { useState } from "react";
+
+// 3. create component 
+const ShoppingList = () => {
+    const [data, setData] = useState(Data)
+    const cartItems = data.map((item) => {
+        console.log(item)
+        return (
+            <li>{item.name}: {" "}{item.price}</li>
+        );
+    }); 
+    
+    return (
+        <div>
+            <h1>Big Time Shopping</h1>
+            {cartItems}
+        </div>
+    );
+};
+
+// 4. export component
+export default ShoppingList;
