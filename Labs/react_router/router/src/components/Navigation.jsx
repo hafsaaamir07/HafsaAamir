e60a9dd7775fbaaa20a550b8e33b654739b92ed8
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-22 12:00:16 -0400

    fixing link tags

--- Labs/react_router/router/src/components/Navigation.jsx
+++ Labs/react_router/router/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
     return (
         <div className="ui secondary pointing menu">
-            <a className="active item" href="/home"> Home </a>
+            <a className="active item" href="/"> Home </a>
             <a className="item" href="/about"> About </a>
             <a className="item" href="/projects"> Projects </a>
             <div className="right menu">
