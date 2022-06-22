5bdafdef51d7d32bd4910563c8951e38e4864e2f
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-21 21:17:30 -0400

    rendering shoppingList

---
+++ Labs/shopping_cart/store/src/components/App.jsx
@@ -0,0 +1,17 @@
+// 1. import react
+import React, { useState } from "react";
+
+// 2. additional imports
+import ShoppingList from "./ShoppingList";
+
+// 3. create components
+const App = () => {
+    return (
+        <div>
+            <ShoppingList />
+        </div>
+    );
+};
+
+// 4. export component
+export default App;
