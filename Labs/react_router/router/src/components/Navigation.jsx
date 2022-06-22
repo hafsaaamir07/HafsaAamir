b4502ecaad13311e0cbdf738b064f934bf901a70
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-22 11:34:01 -0400

    fixing errors

--- Labs/react_router/router/src/components/Navigation.jsx
+++ Labs/react_router/router/src/components/Navigation.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <div class="ui secondary pointing menu">
-            <a class="active item" href="/home"> Home </a>
-            <a class="item" href="/about"> About </a>
-            <a class="item" href="/projects"> Projects </a>
-            <div class="right menu">
-                <a class="ui item"> Logout </a>
+        <div className="ui secondary pointing menu">
+            <a className="active item" href="/home"> Home </a>
+            <a className="item" href="/about"> About </a>
+            <a className="item" href="/projects"> Projects </a>
+            <div className="right menu">
+                <a className="ui item"> Logout </a>
             </div>
         </div>
     );
