92f5cc3c6041a0d7e6aff6e12e06402a20608b6b
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-23 10:46:13 -0400

    set up hooks to use

--- Labs/api_react/api/src/components/Users.jsx
+++ Labs/api_react/api/src/components/Users.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Users = () => {
-    return (
-        <div>
-            <h1>User component</h1>
-        </div>
-    );
+    //lets create a variable to hold our application
+    /*initial state will be ana empty array because before
+    we make our api call its going to empty */
+    const [users, setUsers] = useState()
+
+    //lets craete our effect
+    useEffect (() => {
+        return(
+            <div>
+                <h1>Users Component</h1>
+            </div>
+        )
+
+    });
 };
 
 export default Users;
