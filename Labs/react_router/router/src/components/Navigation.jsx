1dc5deb87239eb406284b0bcca1a79eaec48b3ed
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-22 23:21:20 -0400

    sending props

--- Labs/react_router/router/src/components/Navigation.jsx
+++ Labs/react_router/router/src/components/Navigation.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-const Navigation = () => {
+const Navigation = (props) => {
+    console.log(props)
     return (
        //using link tags instaed of anchor bc anchor tags will reload the application
        //and we want react to re-render/re-load
        //with link tags you do not need hrefs
        //we are basically saying link the home page "to" this..
+
         <div className="ui secondary pointing menu">
+            {/*the actice class is the one with the line under indicating this is the page clicked */}
             <Link className="active item" to="/"> Home </Link>
             <Link className="item" to="/about"> About </Link>
             <Link className="item" to="/projects"> Projects </Link>
