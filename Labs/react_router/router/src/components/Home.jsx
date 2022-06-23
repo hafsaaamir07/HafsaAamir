f371838817f88058add6e6daea5bcf9bfd1d3691
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-22 23:15:09 -0400

    console logging props

--- Labs/react_router/router/src/components/Home.jsx
+++ Labs/react_router/router/src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const Home = (props) => {
     console.log(props)
-    
     return(
         <div>
             <h1>Home Component</h1>
--- Labs/react_router/router/src/components/Projects.jsx
+++ Labs/react_router/router/src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Projects = () => {
+const Projects = (props) => {
+    console.log(props);
     return(
         <div>
             <h1>Projects Component</h1>
