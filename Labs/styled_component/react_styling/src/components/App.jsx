3637c4816bce89682f4d0ced5acfa258661b73ce
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-23 03:43:42 -0400

    passing props to IsEven

--- Labs/styled_component/react_styling/src/components/App.jsx
+++ Labs/styled_component/react_styling/src/components/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
                     <Greeting />
                 </Title>
             </Component>
-            <IsEven />
+            <IsEven number={12}/>
         </div>
     );
 };
