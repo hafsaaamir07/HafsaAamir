97341e6e726c2eeaa0c6c3269447b79a0f1eb834
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-27 19:52:28 -0400

    created and imported state context

---
+++ Labs/state_management/use_reducer/src/contexts/StateContext.jsx
@@ -0,0 +1,8 @@
+// 1. import create context
+import { createContext } from "react";
+
+// 2. create your context object
+const context = createContext(null);
+
+// 3. export your context object
+export default context;
