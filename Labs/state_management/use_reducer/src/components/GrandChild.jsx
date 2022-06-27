8b1599cb3adc2fe354c247c6ab23acfb32db59ec
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-27 19:48:21 -0400

    created and up set new component

---
+++ Labs/state_management/use_reducer/src/components/GrandChild.jsx
@@ -0,0 +1,16 @@
+// 1. import react
+import React from "react";
+
+// 2. additional imports
+
+// 3. create component
+const GrandChild = () => {
+    return (
+        <div>
+            <h1>GrandChild Component</h1>
+        </div>
+    )
+}
+
+// 4. export component
+export default GrandChild;
