40456223b03a3df688ae052ef7d0d6af2955b812
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-27 19:55:19 -0400

    wrapping child component in StateContext.Provider

--- Labs/state_management/use_reducer/src/components/Parent.jsx
+++ Labs/state_management/use_reducer/src/components/Parent.jsx
@@ -3,12 +3,16 @@ import React from "react";
 
 // 2. additional imports
 import Child from "./Child";
+import StateContext from "./../contexts/StateContext"
+import { useState } from "react";
 
 // 3. create component
 const Parent = () => {
     return (
         <div>
-            <Child/>
+            <StateContext.Provider>
+                <Child/>
+            </StateContext.Provider>
         </div>
     )
 }
