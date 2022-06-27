33dc903920a444e3ea2bdbfa8934bcd760460c02
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-27 19:46:09 -0400

    created and steup components

---
+++ Labs/state_management/use_reducer/src/components/Child.jsx
@@ -0,0 +1,16 @@
+// 1. import react
+import React from "react";
+
+// 2. additional imports
+
+// 3. create component
+const Child = () => {
+    return (
+        <div>
+            <h1>Parent Component</h1>
+        </div>
+    )
+}
+
+// 4. export component
+export default Child;
---
+++ Labs/state_management/use_reducer/src/components/Parent.jsx
@@ -0,0 +1,16 @@
+// 1. import react
+import React from "react";
+
+// 2. additional imports
+
+// 3. create component
+const Parent = () => {
+    return (
+        <div>
+            <h1>Parent Component</h1>
+        </div>
+    )
+}
+
+// 4. export component
+export default Parent;
