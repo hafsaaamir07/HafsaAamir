8c69b89cd89d0d95fab70c82aa64bb5e895f625a
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-27 23:52:48 -0400

    attached function to onclick in btn

--- Labs/state_management/use_reducer/src/components/GrandChild.jsx
+++ Labs/state_management/use_reducer/src/components/GrandChild.jsx
@@ -9,11 +9,35 @@ import StateContext from "./../contexts/StateContext"
 const GrandChild = () => {
     const ctx = useContext(StateContext)
     console.log(ctx) // state and dispatch coming from Parent component
+    
+    // craete a function responsible for dispatching our addition action
+    const dispatchAdditionAction = (obj) => {
+        return ctx.dispatch(obj)
+    }
+
+    //create a function responsible for dispatching our subtraction action
+    const dispatchSubtractionAction = (obj) => {
+        return ctx.dispatch(obj)
+    }
+    
+    
+    
+    
+    
     return (
         <div>
+            {/*h1 diaplys the state pulled from the context  */}
             <h1>{ctx.state}</h1>
-            <button>Add</button>
-            <button>Subtract</button>
+            {/*the buttons call dispatch and pass the cation to the reducer */}
+            <button onClick={()=> {
+                return dispatchAdditionAction({type: "ADD", payload: null})
+                }}>Add
+            </button>
+            <button onClick={() => {
+                return dispatchSubtractionAction ({type:"SUBTRACT", payload: null})
+                }}
+                >Subtract
+            </button>
         </div>
     )
 }
