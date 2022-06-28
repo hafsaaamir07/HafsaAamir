8347f6224da070f933d51984e727bcaac9ca9b46
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-27 20:04:40 -0400

    getting useState in our conetxt object

--- Labs/state_management/use_reducer/src/components/GrandChild.jsx
+++ Labs/state_management/use_reducer/src/components/GrandChild.jsx
@@ -2,9 +2,12 @@
 import React from "react";
 
 // 2. additional imports
+import { useContext } from "react";
+import StateContext from "./../contexts/StateContext"
 
 // 3. create component
 const GrandChild = () => {
+    const ctx = useContext(StateContext)
     return (
         <div>
             <h1>GrandChild Component</h1>
