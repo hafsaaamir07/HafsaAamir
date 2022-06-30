e935d4883cd896fed142a5683af1db73d32e3131
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-29 22:10:31 -0400

    sending data as props to Giphy component

--- Labs/gif_app/giphy/src/components/Giphy.jsx
+++ Labs/gif_app/giphy/src/components/Giphy.jsx
@@ -3,23 +3,19 @@ import { useState } from "react";
 import axios from "axios";
 import Display from "./Display";
 
-
-
 const Giphy = () => {
-
-    
     const [gif , setGif] = useState("")
     const handleClick = async () => {
         const response = await axios.get("https://api.giphy.com/v1/gifs/random?api_key=IwSOpWzXHv4qCzM4HJ78xHguc90ME0Su&tag=&rating=g")
-        const data = response.data.data.url
+        const data = response.data.data.images.original.url
         console.log(data)
-        setGif(response)
+        setGif(data)
     }
     return (
         <div>
             <h1>Giphy</h1>
             <button onClick={handleClick}>Make API CALL Again</button>
-            <Display gif={gif}/>
+            <Display img={gif}/>
         </div>
     )
 }
