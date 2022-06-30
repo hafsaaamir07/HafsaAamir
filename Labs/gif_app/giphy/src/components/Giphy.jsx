86d4aeec03bed1665fd96ab4293aff3c5a1f9326
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-29 21:13:44 -0400

    created useState

--- Labs/gif_app/giphy/src/components/Giphy.jsx
+++ Labs/gif_app/giphy/src/components/Giphy.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import env from "react-dotenv";
 
 const Giphy = () => {
+    env.config({
+        path: "./config.env"
+    });
+    const [gif , setGif] = useState("")
+
     return (
         <div>
             <h1>Giphy</h1>
+            <button>Make API CALL Again</button>
         </div>
     )
 }
