08441345889dd086c425d7cd31a3270b3258f15f
hafsaaamir07 <hafsaaamir07@gmail.com>
2022-06-29 21:42:21 -0400

    created onclick and attched to button

--- Labs/gif_app/giphy/src/components/Giphy.jsx
+++ Labs/gif_app/giphy/src/components/Giphy.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { useState } from "react";
-import { useEffect } from "react";
-import env from "react-dotenv";
+import axios from "axios";
+import Display from "./Display";
+
+
 
 const Giphy = () => {
-    env.config({
-        path: "./config.env"
-    });
-    const [gif , setGif] = useState("")
 
+    
+    const [gif , setGif] = useState("")
+    const handleClick = async () => {
+        const response = await axios.get("https://api.giphy.com/v1/gifs/random?api_key=IwSOpWzXHv4qCzM4HJ78xHguc90ME0Su&tag=&rating=g")
+        const data = response.data.data.url
+        console.log(data)
+        setGif(response)
+    }
     return (
         <div>
             <h1>Giphy</h1>
-            <button>Make API CALL Again</button>
+            <button onClick={handleClick}>Make API CALL Again</button>
+            <Display gif={gif}/>
         </div>
     )
 }
